0401eee599ae250c0eb746e9aec2cf0c7fab98e7
Alexander Debelyi <debelyy@yandex.ru>
2019-05-15 00:58:06 +0500

    auth

---
+++ PET.Infrastucture/UserDataService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PET.Application.Services;
+using PET.Domain.Models;
+using PET.Ef.DbContexts;
+
+namespace PET.Infrastructure
+{
+    public class UserDataService : Repository<User>, IDataService<User>
+    {
+        public UserDataService(UserDbContext context) : base(context)
+        {
+        }
+
+        protected override IQueryable<User> ConfigureQuery()
+        {
+            return dbSet;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await ConfigureQuery().ToArrayAsync();
+        }
+    }
+}
